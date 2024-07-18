31f68ccb616ecb7b2faae1a5395969b50ffe67c8
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-18 09:36:24 +0300

    Blocks change color now

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const canvas = document.querySelector(".canvas");
 for(var i = 0; i < 256; i++){
   const block = document.createElement("div");
   block.setAttribute("class", "block");
+  block.addEventListener("mouseover", () => block.style.backgroundColor = "black");
   canvas.appendChild(block);
 }
 
