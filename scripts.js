b0c4e54184b985314fa8d5b55a89d5abc066d7b9
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-18 10:40:33 +0300

    Working as intended

--- scripts.js
+++ scripts.js
@@ -1,11 +1,36 @@
-
 const canvas = document.querySelector(".canvas");
+const button = document.querySelector("#canvasButton");
 
-for(var i = 0; i < 256; i++){
-  const block = document.createElement("div");
-  block.setAttribute("class", "block");
-  block.addEventListener("mouseover", () => block.style.backgroundColor = "black");
-  canvas.appendChild(block);
+button.addEventListener("click", buttonPress)
+
+function buttonPress() {
+  let size = prompt("How many pixels on canvas", "16");
+  if(Number(size)) {
+    let blocksPerWidth = Number(size);
+    canvasConstructor(blocksPerWidth);
+  } else {
+    buttonPress();
+  }
 }
 
+function canvasConstructor(blockNum) {
+  //Canvas width is 960px
+  //Calculation for the width of an individual block
+  const blockSize = (Math.sqrt((Math.pow(960, 2)) / (Math.pow(blockNum, 2))));
+  console.log(canvas.childElementCount)
+  
+  while(canvas.childElementCount > 0) {
+    for(child of canvas.childNodes) {
+      canvas.removeChild(child);
+    }
+  }
 
+  for(var i = 0; i < (blockNum*blockNum); i++){
+    const block = document.createElement("div");
+    block.setAttribute("class", "block");
+    block.style.height = `${blockSize}px`;
+    block.style.width = `${blockSize}px`;
+    block.addEventListener("mouseover", () => block.style.backgroundColor = "black");
+    canvas.appendChild(block);
+  }
+}
