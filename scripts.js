ec8e6598a8be61e99b283f1441e0bb6f30841464
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-18 09:01:24 +0300

    Grid of Divs

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 
 const canvas = document.querySelector(".canvas");
 
+for(var i = 0; i < 256; i++){
+  const block = document.createElement("div");
+  block.setAttribute("class", "block");
+  canvas.appendChild(block);
+}
 
-const block = document.createElement("div");
-block.setAttribute("class", "block");
-
-canvas.appendChild(block);
 
